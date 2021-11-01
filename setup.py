b09855befcb2c5ca6a9320835bee754fd66281dd
aladin08 <alajavateacher@gmail.com>
2021-11-01 18:23:05 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='an end to end machine learning projtect',
+    author='alaeddine',
+    license='',
+)
